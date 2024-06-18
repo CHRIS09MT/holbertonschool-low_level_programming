c10abd589451b02558c2d33b25ce4983caff89f2
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-06-18 13:17:44 -0500

    print the alphabet in lowercase and uppercase

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Start pointer
+ * Return: return 0
+ */
+
+int main(void)
+{
+	char a, b;
+
+	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
+	{
+		for (b = 'A'; b <= 'Z'; b++)
+			putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}
