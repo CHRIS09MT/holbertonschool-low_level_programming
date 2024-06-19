ad2a92266766c19db8a249e9a618dbc23c11f039
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-06-18 21:17:59 -0500

    print numbers combinations

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Start pointer
+ * Return: return 0
+ */
+
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = a + 1; b <= 9; b++)
+		{
+			putchar(a + '0');
+			putchar(b + '0');
+
+			if ((a != 8) || (b != 9))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
