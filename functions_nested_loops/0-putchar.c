807763dc4611b120d8f1e262f5ebce5f905acb40
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-06-19 10:10:22 -0500

    print the text _putchar wearing only the funtion putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 #include "main.h"
 
 /**
@@ -8,14 +8,6 @@
 
 int main(void)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
+	write(STDOUT_FILENO, "_putchar\n", 9);
 	return (0);
 }
