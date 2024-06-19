51d88c5edfe2a6ed597e97ad59eb35af8ae5b4f1
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-06-19 10:01:53 -0500

    print the text _putchar wearing only the funtion putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Start pointer
+ * Return: return 0
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
