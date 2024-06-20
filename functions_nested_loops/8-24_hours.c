8b386c6cdbf338b72a4d0611ed5a88fd230fd761
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-06-20 11:11:55 -0500

    print a clockie than starting from 00:00 to 23:59

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include <time.h>
+#include "main.h"
+
+/**
+ * jack_bauer - print a clockie, starting from 00:00 to 23:59
+ * Return: Always 0
+ */
+
+void jack_bauer(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 23; a++)
+	{
+		for (b = 0; b <= 59; b++)
+		{
+			_putchar((a / 10) + '0');
+			_putchar((a % 10) + '0');
+			_putchar(':');
+			_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+			_putchar(' ');
+			_putchar('\n');
+		}
+	}
+}
