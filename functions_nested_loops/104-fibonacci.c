333c702a2f3a1460631a9c89e61b868f2ecd6fc8
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-06-22 15:44:36 -0500

    print the numbers in format Fibonacci to 100

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -10,18 +10,14 @@ int main(void)
 	int s;
 	unsigned long x = 1, y = 1, i;
 
-	printf("%lu, ", x);
+	printf("%lu", x);
 
 	for (s = 3; s <= 100; s++)
 	{
 		i = x + y;
+		printf(", %lu", i);
 		x = y;
 		y = i;
-
-		printf("%lu", y);
-
-		if (s < 100)
-			printf(", ");
 	}
 	printf("\n");
 	return (0);
