3b5f7acdf41eb2e590c579ba6c03d50d67b152fe
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-06-22 15:05:41 -0500

    print the numbers in format Fibonacci

--- functions_nested_loops/103-fibonacci.c
+++ functions_nested_loops/103-fibonacci.c
@@ -10,18 +10,14 @@ int main(void)
 	int s;
 	unsigned long x = 1, y = 1, i;
 
-	printf("%lu, %lu, ", x, y);
-
 	for (s = 3; s <= 34; s++)
 	{
 		i = x + y;
 		x = y;
 		y = i;
 
-		printf("%lu", y);
-
-		if (s < 34)
-			printf(", ");
+		if (s == 33)
+			printf("%lu", y);
 	}
 	printf("\n");
 	return (0);
