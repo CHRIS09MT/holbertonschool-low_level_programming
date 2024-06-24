84c2c86d35c2aba789d9207276aa0efc6c0eadd3
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-06-24 13:30:26 -0500

    printing a sapce and diagonal

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draw lines diagonals and a space
+ * @n: Amount the times to print
+ */
+
+void print_diagonal(int n)
+{
+	char a, b;
+
+	for (a = 0; a < n; a++)
+	{
+		for (b = 0; b < a; b++)
+			_putchar(' ');
+
+		_putchar('\\');
+
+			if (a == n -1)
+			continue;
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
