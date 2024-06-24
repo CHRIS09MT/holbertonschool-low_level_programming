77e69d940217bc3dd63978c83a8b77d5219f3839
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-06-24 10:42:51 -0500

    printing the numbers the 0 to 9, ignoring the 2 and 4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -9,9 +9,7 @@ void print_most_numbers(void)
 	char a;
 
 	for (a = 0; a <= 9; a++)
-		{
-			if ((a != 2) && (a != 4))
-				_putchar(a + '0');
-		}
+		if ((a != 2) && (a != 4))
+			_putchar(a + '0');
 	_putchar('\n');
 }
