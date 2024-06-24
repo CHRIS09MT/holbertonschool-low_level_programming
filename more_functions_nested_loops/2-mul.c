bc6bf4b1805207caa15f4659c09b9b6dd8263596
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-06-24 10:15:38 -0500

    return the result the two numbers

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * mul - Start pointer
+ * mul - multiply two numbers y return the result
  * @a: first number to multiply
  * @b: second number to multiply
+ * Return: return the result the of multiplication of two numbers
  */
 
 int mul(int a, int b)
