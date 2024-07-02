3afb0d1c285b0e7560cdf6bedf2230881de6453b
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-07-02 11:41:14 -0500

    llenando bytes

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memset - start pointer
+ * @s: pointer to fills memory
+ * @b: chars
+ * @n: amount the print
+ * Return: return the pointer s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+		s[i] = b;
+	return (s);
+}
