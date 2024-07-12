51755467a1d6120de8952f0c6eb5890871479e1a
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-07-11 20:15:28 -0500

    ojala

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - Crea un array
+ * @min: Rango mínimo
+ * @max: Rango máximo
+ * Return: Return the arrays
+ */
+
+int *array_range(int min, int max)
+{
+	int *array;
+	int b, c = 0;
+
+    /** esto verifica que 'min' no sea mayor que 'max' */
+ 	if (min > max)
+		return (NULL);
+
+	/** aquí se asigna memoria para 'array' */
+	array = malloc(sizeof(*array) * ((max - min) + 1));
+
+    /** se llena 'array' llevando 'min' hasta 'max' */
+	if (array != NULL)
+	{
+		for (b = min; b <= max; b++)
+		{
+			array[c] = b;
+			c++;
+		}
+		return (array);
+	}
+	else /** retorna NULL si hay un error */
+		return (NULL);
+}
