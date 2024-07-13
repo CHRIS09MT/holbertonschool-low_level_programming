f11510ed1a30ef4e02642721c387b22e30714265
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-07-13 18:52:10 -0500

    excesos

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -12,7 +12,7 @@ void print_dog(struct dog *d)
 	{
 		char *name = d->name ? d->name : "(nil)";
 		float age = d->age ? d->age : 0;
-		char *owner = d->owner ? d->owner : "nil";
+		char *owner = d->owner ? d->owner : "(nil)";
 
 		printf("Name: %s\nAge: %f\nOwner: %s\n", name, age, owner);
 	}
