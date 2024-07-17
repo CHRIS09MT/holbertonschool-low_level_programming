54e8a18e6a95eb8176bb471320a9a050b3d93193
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-07-17 15:06:58 -0500

    terminando punto 2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @cmp: function which compare values
+ * @array: array to print
+ * @size: size of arrays
+ * Return: returns -1 if no element
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int b = 0;
+
+	if (array == NULL && cmp == NULL)
+		return (-1);
+
+	for (; b < size; b++)
+	{
+		if (cmp(array[b]) != 0)
+			return (b);
+	}
+
+	return (-1);
+}
