fbe46d0e2bf2ed6c8c951459089a72fd08d1dc75
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-07-20 14:39:46 -0500

    add of parameters of functions

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -2,6 +2,10 @@
 #include "variadic_functions.h"
 
 /**
+ * sum_them_all - this program adds all parameters of the function
+ * and return the result
+ * @n: parameter
+ * Return: return the add of all the parameters
  */
 
 int sum_them_all(const unsigned int n, ...)
@@ -9,6 +13,7 @@ int sum_them_all(const unsigned int n, ...)
 	va_list args;
 	int ac = 0;
 	unsigned int ope = 0;
+
 	va_start(args, n);
 
 	if (n != 0)
@@ -16,11 +21,12 @@ int sum_them_all(const unsigned int n, ...)
 		for (; ope < n; ope++)
 		{
 			int sig = va_arg(args, int);
+
 			ac += sig;
 		}
 	}
 	else
-		return 0;
+		return (0);
 
 	va_end(args);
 
