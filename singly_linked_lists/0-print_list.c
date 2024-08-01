6f28488d48320bbf9498c055e2af37f2e754e36f
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-07-31 20:46:07 -0500

    runing the list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+* print_list - print the node list
+* @h: pointer to the head of the list
+* Return: the number of nodes in the list
+ */
+
+size_t print_list(const list_t *h)
+{
+    size_t cant = 0;
+    const list_t *nods = h;
+
+    while (nods != NULL) /** si la lista no es NULL, se recorre la lista. */
+    {
+        printf("[%d] ", nods->len); /** se imprime la longitud de la lista. */
+
+        if (nods->str != NULL)
+            printf("%s", nods->str); /** imprimo la lista */
+        else
+            printf("(nil)");
+
+        printf("\n");
+        nods = nods->next; /** se mueve al siguiente nodo de la lista. */
+        cant++; /** se aumenta el contador. */
+    }
+    return (cant); /** se retorna la cantidad de nodos */
+}
