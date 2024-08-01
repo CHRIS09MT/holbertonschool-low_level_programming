e051c369297fbb804ccb1f1b5fe9953a7daa4353
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-07-31 21:36:28 -0500

    print the numbers of elements the of list

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* list_len - print the length of the list
+* @h: head of the list
+* Return: length of the list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t len = 0;
+	const list_t *nods = h;
+
+	while (nods)
+	{
+		len++;
+		nods = nods->next;
+	}
+	return (len);
+}
