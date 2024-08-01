d992a7f2e4923352de9880205754b5efb6a74b8e
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-07-31 21:08:17 -0500

    arranging betty

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -9,21 +9,21 @@
 
 size_t print_list(const list_t *h)
 {
-    size_t cant = 0;
-    const list_t *nods = h;
+size_t cant = 0;
+const list_t *nods = h;
 
-    while (nods != NULL) /** si la lista no es NULL, se recorre la lista. */
-    {
-        printf("[%d] ", nods->len); /** se imprime la longitud de la lista. */
+while (nods != NULL) /** si la lista no es NULL, se recorre la lista. */
+{
+printf("[%d] ", nods->len); /** se imprime la longitud de la lista. */
 
-        if (nods->str != NULL)
-            printf("%s", nods->str); /** imprimo la lista */
-        else
-            printf("(nil)");
+if (nods->str != NULL)
+printf("%s", nods->str); /** imprimo la lista */
+else
+printf("(nil)");
 
-        printf("\n");
-        nods = nods->next; /** se mueve al siguiente nodo de la lista. */
-        cant++; /** se aumenta el contador. */
-    }
-    return (cant); /** se retorna la cantidad de nodos */
+printf("\n");
+nods = nods->next; /** se mueve al siguiente nodo de la lista. */
+cant++; /** se aumenta el contador. */
+}
+return (cant); /** se retorna la cantidad de nodos */
 }
