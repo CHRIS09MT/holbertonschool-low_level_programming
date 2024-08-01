4d0ba0527f3895e818a11041271a38046f8a1154
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-08-01 14:02:06 -0500

    creating a new node the start the of list

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -11,7 +11,7 @@ size_t list_len(const list_t *h)
 	size_t len = 0;
 	const list_t *nods = h;
 
-	while (nods)
+	while (nods != NULL)
 	{
 		len++;
 		nods = nods->next;
---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node = malloc(sizeof(list_t));
+	int length = 0;
+
+	if (!new_node)
+		return (NULL);
+
+	new_node->str = strdup(str);
+
+	if (!new_node->str)
+	{
+		free(new_node);
+                return (NULL);
+	}
+	while (str[length++] != '\0')
+		;
+	new_node->len = length - 1;
+	new_node->next = *head;
+	*head = new_node;
+
+	return(new_node);
+}
