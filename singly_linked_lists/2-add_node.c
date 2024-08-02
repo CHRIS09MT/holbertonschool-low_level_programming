b227d1ef1a070aa76c459affe2d35cc8d99f6b00
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-08-02 18:18:41 -0500

    actualizing all

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,24 +11,19 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node = malloc(sizeof(list_t));
-	int length = 0;
+	list_t *new_node = NULL;
+	new_node = malloc(sizeof(list_t));
 
 	if (!new_node)
 		return (NULL);
 
-	new_node->str = strdup(str);
+	new_node->str = strdup(str); /** se duplica la string a guardar */
+	new_node->len = strlen(str); /** mide la string guardada */
+	new_node->next = NULL; /** crea un nodo */
 
-	if (!new_node->str)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	while (str[length++] != '\0')
-		;
-	new_node->len = length - 1;
-	new_node->next = *head;
-	*head = new_node;
+	if (*head != NULL)
+		new_node->next = *head; /** añade el nuevo nodo al principio de la lista */
 
+	*head = new_node; /** actualiza el puntero al principio de la lista */
 	return (new_node);
 }
--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -12,38 +12,27 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *end = *head;
-	list_t *new_node = malloc(sizeof(list_t));
-	int lenght = 0;
+	list_t *end = NULL;
+	list_t *new_node = NULL;
+	new_node = malloc(sizeof(list_t));
 
 	if (!new_node)
 		return (NULL);
 
 	new_node->str = strdup(str);
-
-	if (!new_node->str)
-	{
-		free(new_node);
-return (NULL);
-	}
-	while (str[lenght++])
-		;
-
-	new_node->len = lenght - 1;
-
-	if (!end)
-	{
-		*head = new_node;
-		end = *head;
-	}
-	else
-	{
-		while (end->next)
-			end = end->next;
-	}
-
-	end->next = new_node;
+	new_node->len = strlen(str);
 	new_node->next = NULL;
 
+	if (*head != NULL)
+		*head = new_node;
+	else
+	{
+		end = *head;
+
+		while (end->next!= NULL)
+			end = end->next;
+
+		end->next = new_node;
+	}
 	return (new_node);
 }
