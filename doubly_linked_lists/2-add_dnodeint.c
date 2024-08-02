c13b531de55a7a158e30317064181ee89be9bd11
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-08-02 16:24:09 -0500

    adding a nodo to start the of head

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
 * add_dnodeint - add a node in the head of the list
@@ -17,13 +18,14 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (!new_node)
 		return (NULL);
 
-	new_node->next = NULL;
+        new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
 
 	if (*head != NULL)
-		new_node->next = *head;
+		(*head)->prev = new_node;
 
 	*head = new_node;
-	new_node->n = n;
 
 	return (new_node);
 }
