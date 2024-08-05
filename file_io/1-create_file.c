bc617a49c1c0b30549ef9c76e7aae6358a83cb22
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-08-05 14:44:00 -0500

    correcting betty the of file 1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -27,9 +27,9 @@ int create_file(const char *filename, char *text_content)
 
 	if (bytes_written == -1)
 	{
-                close(fd);
-                return (-1);
-        }
+		close(fd);
+		return (-1);
+	}
 
 	close(fd);
 	return (1);
