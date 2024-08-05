7e4eb901ab034c209d35de5e320e1f13e3869b98
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-08-05 13:13:31 -0500

    this function creat a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -22,8 +22,8 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (0);
 
-	if (text_content == NULL)
-		bytes_written = write(fd, "", strlen(text_content));
+	if (text_content != NULL)
+		bytes_written = write(fd, text_content, strlen(text_content));
 
 	if (bytes_written == -1)
 	{
