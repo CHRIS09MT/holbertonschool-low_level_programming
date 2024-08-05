d2845f0d73d9f93756da37011d6ba5da785ab503
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-08-05 14:42:04 -0500

    finishing

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,19 +16,20 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	/** A file is created, type write, with permissions 600 */
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 600);
+	/** A file is created, type write, with permissions */
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
 
-	bytes_written = write(fd, text_content, strlen(text_content));
+	if (text_content != NULL)
+		bytes_written = write(fd, text_content, strlen(text_content));
 
 	if (bytes_written == -1)
 	{
-		close(fd);
-		return (-1);
-	}
+                close(fd);
+                return (-1);
+        }
 
 	close(fd);
 	return (1);
