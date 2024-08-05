776dd5ffe6b7751bbfe8d84439a492626ea296d4
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-08-05 14:02:34 -0500

    correcting stuffs

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -20,10 +20,9 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 600);
 
 	if (fd == -1)
-		return (0);
+		return (-1);
 
-	if (text_content != NULL)
-		bytes_written = write(fd, text_content, strlen(text_content));
+	bytes_written = write(fd, text_content, strlen(text_content));
 
 	if (bytes_written == -1)
 	{
