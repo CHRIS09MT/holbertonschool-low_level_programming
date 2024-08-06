ee994f7d1de3a559e2f053260d39d853a7622eec
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-08-06 15:12:34 -0500

    correcting

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -29,15 +29,15 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		close(file_from), exit(99);
 	}
-	while ((bytes_read = read(file_from, buffer, sizeof(buffer))) > 0)
+	do
 	{
+		bytes_read = read(file_from, buffer, sizeof(buffer));
+		if (bytes_read == -1)
+			break;
 		bytes_written = write(file_to, buffer, bytes_read);
-		if (bytes_written == -1 || bytes_written != bytes_read)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(file_from), close(file_to), exit(100);
-		}
-	}
+		if (bytes_written == -1)
+		        exit(99);
+	} while (bytes_written > 0);
 	if (bytes_read == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
